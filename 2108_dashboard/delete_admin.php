83b62ec28e909a9b0400857cd7d5334d985a9c43
eivra-sm <2023044461@feu.edu.ph>
2025-04-09 23:54:15 +0800

    Update delete_admin.php

--- 2108_dashboard/delete_admin.php
+++ 2108_dashboard/delete_admin.php
@@ -4,14 +4,12 @@ include 'db_connection.php';
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
-    // Get admin record from customers_info
     $sql = "SELECT * FROM customers_info WHERE ID = $id AND User_lvl = 2";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         $admin = $result->fetch_assoc();
 
-        // Prepare values for archive
         $original_id = $admin['ID'];
         $fullname = $conn->real_escape_string($admin['Fullname']);
         $user_lvl = intval($admin['User_lvl']);
@@ -21,7 +19,6 @@ if (isset($_GET['id'])) {
         $profile_photo = isset($admin['Profile_Photo']) ? $conn->real_escape_string($admin['Profile_Photo']) : '';
         $archived_at = date('Y-m-d H:i:s');
 
-        // Insert into archive_admin
         $insert_sql = "INSERT INTO archive_admin (
             Original_Customer_ID, Fullname, User_lvl, Birthday, Billing_Address, Pword, Profile_Photo, Archived_At
         ) VALUES (
@@ -29,7 +26,6 @@ if (isset($_GET['id'])) {
         )";
         $conn->query($insert_sql);
 
-        // Delete from customers_info
         $conn->query("DELETE FROM customers_info WHERE ID = $id");
 
         header("Location: admins_archive.php");
